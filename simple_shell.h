83a1476fb6c0d50c8d7477266e153600f85fefdc
KILLA <killa@MBP-de-KILLA.localdomain>
2024-04-24 11:56:44 -0400

    fixing valgrin error and exit

--- simple_shell.h
+++ simple_shell.h
@@ -21,6 +21,5 @@ void print_not_found(char *cmd, char *name);
 int call_command(char *cmd_arr[], char *name);
 int _str_n_cmp(char *s1, char *s2, int n);
 char *_getenv(char *var);
-char *path_not_find(char *cmd);
 
 #endif /* SIMPLE_SHELL_H */
